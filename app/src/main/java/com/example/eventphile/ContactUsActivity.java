00fd20577e50779e95bbfd75be2ee742b339209f
aniruddhapai <agpai2@illinois.edu>
2019-12-06 17:24:23 -0600

    Minor changes in contact

--- app/src/main/java/com/example/eventphile/ContactUsActivity.java
+++ app/src/main/java/com/example/eventphile/ContactUsActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.widget.TextView;
 
 public class ContactUsActivity extends AppCompatActivity {
@@ -11,7 +12,7 @@ public class ContactUsActivity extends AppCompatActivity {
     private String phoneText;
     private String emailText;
     private String webText;
-    private String locationText;
+    //private String locationText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,19 +24,27 @@ public class ContactUsActivity extends AppCompatActivity {
         TextView website = findViewById(R.id.website);
         TextView location = findViewById(R.id.location);
 
-        phoneText = "+1 (217) 111 2222";
-        //emailText = "eventsApp@events.com";
-        //webText = "https://www.ticketmaster.com";
-        locationText = "Champaign, IL";
+        phoneText = "+1 (217) 819 8923";
+        emailText = "aniruddh.g.pai@gmail.com";
+        webText = "https://www.ticketmaster.com";
+        //locationText = "Champaign, IL";
 
         phone.setText(phoneText);
+        Linkify.addLinks(phone, Linkify.ALL);
 
-        email.setText(Html.fromHtml("<a href=mailto: aniruddh.g.pai@gmail.com>aniruddh.g.pai@gmail.com</a>"));
-        email.setMovementMethod(LinkMovementMethod.getInstance());
+        email.setText(emailText);
+        Linkify.addLinks(email, Linkify.ALL);
+        //email.setText(Html.fromHtml("<a href=mailto: aniruddh.g.pai@gmail.com>aniruddh.g.pai@gmail.com</a>"));
+        //email.setMovementMethod(LinkMovementMethod.getInstance());
 
-        website.setText(Html.fromHtml("<a href='https://www.ticketmaster.com'>https://www.ticketmaster.com</a>"));
-        website.setMovementMethod(LinkMovementMethod.getInstance());
+        website.setText(webText);
+        Linkify.addLinks(website, Linkify.ALL);
+        //website.setText(Html.fromHtml("<a href='https://www.ticketmaster.com'>https://www.ticketmaster.com</a>"));
+        //website.setMovementMethod(LinkMovementMethod.getInstance());
 
-        location.setText(locationText);
+        //location.setText(locationText);
+        //Linkify.addLinks(location, Linkify.ALL);
+        location.setText(Html.fromHtml("<a href='https://www.google.com/maps/place/Champaign,+IL/@40.11461,-88.3471495,12z/data=!3m1!4b1!4m5!3m4!1s0x880cd08dde97691b:0x30f57847b475bfcf!8m2!3d40.1164204!4d-88.2433829'>Champaign, IL</a>"));
+        location.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
