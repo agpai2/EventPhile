eb8ba7505b5e22dc1246d1797e32b04765e26bf8
aniruddhapai <agpai2@illinois.edu>
2019-12-09 15:42:14 -0600

    Notification Activity

--- app/src/main/java/com/example/eventphile/NotificationActivity.java
+++ app/src/main/java/com/example/eventphile/NotificationActivity.java
@@ -2,7 +2,9 @@ package com.example.eventphile;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -10,11 +12,18 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+@SuppressWarnings("deprecation")
 public class NotificationActivity extends AppCompatActivity {
+
     private String name;
+    private NotificationCompat.Builder builder;
+    private NotificationManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,25 +35,35 @@ public class NotificationActivity extends AppCompatActivity {
         eventname.setText(name);
 
         Switch setNotification = findViewById(R.id.switch1);
-        setNotification.setOnClickListener((View unused) -> {
+        setNotification.setOnClickListener(unused -> {
             notification(name);
+        });
+
+        Button button = findViewById(R.id.addReminder);
+        button.setOnClickListener(unused -> {
+            Intent intent = new Intent(this, CalenderActivity.class);
+            intent.putExtra("name", name);
+            startActivity(intent);
         });
     }
 
     protected void notification(String name) {
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher_round)
+        builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.appicon)
                 .setContentTitle("EventPhile")
                 .setContentText(name)
-                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
+                .setWhen(System.currentTimeMillis() + 1000);
+        Notification notification = builder.getNotification();
 
-        Intent notifier = new Intent(Intent.ACTION_VIEW);
+        Intent notifier = new Intent(this, NotificationActivity.class);
         PendingIntent contentInfo = PendingIntent.getActivity(this, 0, notifier, PendingIntent.FLAG_UPDATE_CURRENT);
         builder.setContentIntent(contentInfo);
+        builder.build();
+        manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
 
-        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        manager.notify(1, builder.build());
+        notificationManager.notify(0, builder.build());
 
     }
 }
